01e46a3ead6bf910ecb8b4ae2460baf0afd07ae4
ChaosAttractor <lostattractor@gmail.com>
2025-03-23 18:52:57 +0800

    chore: update deps

--- control/netns_utils.go
+++ control/netns_utils.go
@@ -194,8 +194,8 @@ func (ns *DaeNetns) setupRoutingPolicy() (err error) {
 		Flow:              -1,
 		Family:            unix.AF_INET,
 		Table:             table,
-		Mark:              int(consts.TproxyMark),
-		Mask:              int(consts.TproxyMark),
+		Mark:              consts.TproxyMark,
+		Mask:              &consts.TproxyMark,
 	}, {
 		SuppressIfgroup:   -1,
 		SuppressPrefixlen: -1,
@@ -204,8 +204,8 @@ func (ns *DaeNetns) setupRoutingPolicy() (err error) {
 		Flow:              -1,
 		Family:            unix.AF_INET6,
 		Table:             table,
-		Mark:              int(consts.TproxyMark),
-		Mask:              int(consts.TproxyMark),
+		Mark:              consts.TproxyMark,
+		Mask:              &consts.TproxyMark,
 	}}
 
 	for _, rule := range rules {
